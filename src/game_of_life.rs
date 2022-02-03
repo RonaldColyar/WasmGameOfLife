e55041c2e608add59b3b10b51248bafd0c99b8f5
RonaldColyar <phantexcreations@gmail.com>
2022-02-02 18:54:21 -0600

    back after long break

--- src/game_of_life.rs
+++ src/game_of_life.rs
@@ -1,10 +1,13 @@
+use std::convert::TryInto;
+extern crate wasm_bindgen;
 use serde::{Deserialize, Serialize};
+use wasm_bindgen::prelude::*;
 
 /*
 Game of life is popular grid
 problem that forces you to determine
 all cells that need to die based on
-a few sets of rules. 
+a few sets of rules.
 
 It is a popular leetcode problem and
 tests your understanding. To find more
@@ -18,33 +21,118 @@ project template, but this is an original
 implementation.
 */
 
-pub enum CellState{
-    Alive = 1,
-    Dead = 0,
-    WasDeadNowAlive = 2,
-    WasAliveNowDead = 3
-}
-
 #[derive(Deserialize, Serialize)]
-pub struct CellsJson{
-    pub cells:Vec<Vec<u8>>
+pub struct CellsJson {
+    pub cells: Vec<Vec<u8>>,
 }
 
 #[wasm_bindgen]
-pub struct UniverseImpl{
-    pub cells_json:CellsJson,
+pub struct UniverseImpl {
+    cells_json: CellsJson,
     //we use these indexes
     //to keep track of each
     //past index for ticking
     //we change each
-    pub last_col_index:usize,
-    pub last_row_index:usize,
+    col_index: usize,
+    row_index: usize,
+    //we are storing all possible combinations of
+    //neighbors as array pairs like for example
+    //UP = [1,0]
+    possible_neighbors: [[i8; 2]; 8],
+    //js uses this to determine if ticking should stop
+    pub complete: bool,
 }
 
-impl UniverseImpl{
-   pub fn new(cells:String)->Self{
-       Self{
-        cells:serde_json::from_str(&cells).unwrap()
-       }
-   }
+impl UniverseImpl {
+    pub fn new(cells: String) -> Self {
+        Self {
+            cells_json: serde_json::from_str(&cells).unwrap(),
+            col_index: 0,
+            row_index: 0,
+            possible_neighbors: [
+                [1, 0],   //down
+                [-1, 0],  //up
+                [0, 1],   //right
+                [0, -1],  //left
+                [1, 1],   //down right
+                [-1, -1], //top left
+                [1, -1],  //bottom left
+                [-1, 1],
+            ], //top right
+            complete: false,
+        }
+    }
+
+    pub fn tick(&mut self) {
+        self.col_index += 1;
+        if !(0..self.cells_json.cells[0].len()).contains(&self.col_index) {
+            //ensure there is another row
+            if (0..self.cells_json.cells.len()).contains(&(self.row_index + 1)) {
+                self.col_index = 0;
+                self.row_index = 1;
+            } else {
+                self.complete = true;
+                return;
+            }
+            return;
+        }
+
+        let current_count = self.get_live_neighbor_count();
+        if self.cells_json.cells[self.row_index][self.col_index] == 1 {
+            //if it doesn't have 2 or 3 neighbors it dies
+            if current_count != 2 || current_count != 3 {
+                self.cells_json.cells[self.row_index][self.col_index] = 3;
+            }
+        } else {
+            if current_count == 3 {
+                self.cells_json.cells[self.row_index][self.col_index] = 2;
+            }
+        }
+    }
+
+    // 0 = Dead
+    // 1 = Alive
+    // 2 = WasDeadNowAlive
+    // 3 = WasAliveNowDead
+    fn get_live_neighbor_count(&self) -> usize {
+        let mut live_neighbor_count = 0;
+        for possible_neighbor in self.possible_neighbors {
+            let temp_row = self.row_index as i8;
+            let temp_col = self.col_index as i8;
+            let current_possible_row: i8 = possible_neighbor[0] + temp_row;
+            let current_possible_col: i8 = possible_neighbor[1] + temp_col;
+
+            if current_possible_row >= 0 && current_possible_col >= 0 {
+                let curr_row_as_usize: usize = current_possible_row.try_into().unwrap();
+                let curr_col_as_usize: usize = current_possible_col.try_into().unwrap();
+
+                // if this new neighbor is in range and its alive
+                if (0..self.cells_json.cells.len()).contains(&curr_row_as_usize)
+                    && (0..self.cells_json.cells[0].len()).contains(&curr_col_as_usize)
+                {
+                    if self.cells_json.cells[curr_row_as_usize][curr_row_as_usize] == 2
+                        || self.cells_json.cells[curr_row_as_usize][curr_row_as_usize] == 1
+                    {
+                        live_neighbor_count += 1;
+                    }
+                }
+            }
+        }
+        return live_neighbor_count;
+    }
+
+    pub fn render(&self) -> String {
+        let mut starting_string: String = String::from("");
+        for row in &self.cells_json.cells {
+            for column in row {
+                if column == &(2 as u8) || column == &(1 as u8) {
+                    starting_string.push_str("◼");
+                } else {
+                    starting_string.push_str("◻");
+                }
+            }
+            starting_string.push_str("/n");
+        }
+        return starting_string;
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 extern crate cfg_if;
+extern crate serde;
+extern crate serde_json;
 extern crate wasm_bindgen;
 extern crate web_sys;
-
+mod game_of_life;
 mod utils;
-
